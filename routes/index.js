a07122e26d3a6ea02cf87c65cc0c0e99c60e34ba
Gabriel Nilsson Högberg <gabrielnilsson.hogb@elev.ga.ntig.se>
2024-01-16 11:04:53 +0100

    POST req, Images and story

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,35 @@ const story = require('../data/story.json')
 
 router.get('/', function (req, res) {
   console.log(story.parts[0])
-  res.render('index.njk', { title: 'Welcome', part: story.parts[0] })
+  res.render('index.njk', {
+    username: req.session.username,
+    title: 'Welcome', part: story.parts[0],
+    part: story.parts[0],
+  })
+})
+
+router.post('/username', function (req, res) {
+  req.session.username = req.body.username
+  console.log(req.session.username)
+  res.redirect('/story/0')
+  res.render('part.njk', {
+    username: req.session.username,
+    title: part.name,
+    part: part,
+  })
 })
 
 router.get('/story/:id', function (req, res) {
-  const part = story.parts.find((part) => part.id === parseInt(req.params.id))
+  let part = story.parts.find((part) => part.id === parseInt(req.params.id))
   if (!part) {
     res.status(404).render('404.njk', { title: '404' })
     return
   }
+
+  const name = part.name.replace('[PLAYER]', req.session.username)
+
+  part = { ...part, name: name }
+
   res.render('part.njk', { title: part.name, part: part })
 })
 
