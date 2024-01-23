2f9dbb636074693ac566df9cfff5a7e35478956e
Gabriel Nilsson Högberg <gabrielnilsson.hogb@elev.ga.ntig.se>
2024-01-23 10:26:17 +0100

    route

--- routes/index.js
+++ routes/index.js
@@ -37,4 +37,17 @@ router.get('/story/:id', function (req, res) {
   res.render('part.njk', { title: part.name, part: part })
 })
 
+
+const pool = require('../db')
+
+router.get('/dbtest', async (req, res) => {
+  try {
+    const [parts] = await pool.promise().query('SELECT * FROM gabriel_part')
+    res.json({ parts })
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+})
+
 module.exports = router
